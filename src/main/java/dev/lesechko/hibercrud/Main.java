5c39d694f40c962c7b139df76289d8cb76fb1ede
polo <polo-ru@yandex.ru>
2023-12-11 23:31:22 +0300

    Flyway setup

--- src/main/java/dev/lesechko/hibercrud/Main.java
+++ src/main/java/dev/lesechko/hibercrud/Main.java
@@ -1,7 +1,26 @@
 package dev.lesechko.hibercrud;
 
+//import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
+        String DB_URL = "jdbc:postgresql://localhost:5432/hibercrud";
+        String USER = "postgres";
+        String PASSWORD = "";
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD)) {
+            if (connection != null) {
+                System.out.println("Connected to POSTGRESQL");
+            } else {
+                System.out.println("Connection failed.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("Hello world!");
+
     }
 }
