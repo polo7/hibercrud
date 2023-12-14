32211ddae7f1437f682ae195f29516013fcdee44
polo <polo-ru@yandex.ru>
2023-12-14 18:14:15 +0300

    edits

--- src/main/java/dev/lesechko/hibercrud/Main.java
+++ src/main/java/dev/lesechko/hibercrud/Main.java
@@ -1,9 +1,9 @@
 package dev.lesechko.hibercrud;
 
-//import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
+
 
 public class Main {
     public static void main(String[] args) {
