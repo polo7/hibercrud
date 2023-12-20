8da3a93908d9c895c8375a7123682b6f7387aa5b
polo <polo-ru@yandex.ru>
2023-12-20 16:28:48 +0300

    Finished Labels

--- src/main/java/dev/lesechko/hibercrud/repository/hibernate/HibernateLabelRepositoryImpl.java
+++ src/main/java/dev/lesechko/hibercrud/repository/hibernate/HibernateLabelRepositoryImpl.java
@@ -1,38 +1,32 @@
 package dev.lesechko.hibercrud.repository.hibernate;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.LinkedList;
 import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import dev.lesechko.hibercrud.model.Label;
 import dev.lesechko.hibercrud.model.Status;
-import dev.lesechko.hibercrud.utils.HibernateConnectionUtils;
 import dev.lesechko.hibercrud.repository.LabelRepository;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+import dev.lesechko.hibercrud.utils.HibernateConnectionUtils;
 
 
 public class HibernateLabelRepositoryImpl implements LabelRepository {
     @Override
     public Label save(Label label) {
-
-
-//        String sql = "INSERT INTO labels (name, status) VALUES (?, ?)";
-//        try (PreparedStatement stmnt = DonnectionUtils.getPreparedStatement(sql)) {
-//            stmnt.setString(1, label.getName());
-//            stmnt.setString(2, label.getStatus().name());
-//            if (stmnt.executeUpdate() == 0) return null;
-//            ResultSet generatedKeys = stmnt.getGeneratedKeys();
-//            generatedKeys.next();
-//            label.setId(generatedKeys.getLong(1));
-//            return label;
-//        } catch (SQLException | NullPointerException e) {
-//            e.printStackTrace();
-//            return null;
-//        }
-        return null;
+        Transaction transaction = null;
+        try (Session session = HibernateConnectionUtils.getNewSession()) {
+            transaction = session.beginTransaction();
+            session.persist(label);
+            transaction.commit();
+            return label;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+                System.err.println("Rolling back transaction");
+            }
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
@@ -41,7 +35,7 @@ public class HibernateLabelRepositoryImpl implements LabelRepository {
         Transaction transaction = null;
         try (Session session = HibernateConnectionUtils.getNewSession()) {
             transaction = session.beginTransaction();
-            List<Label> labels = session.createQuery(hql).list();
+            List<Label> labels = session.createQuery(hql, Label.class).list();
             transaction.commit();
             return labels;
         } catch (Exception e) {
@@ -108,14 +102,5 @@ public class HibernateLabelRepositoryImpl implements LabelRepository {
             e.printStackTrace();
             return false;
         }
-//        String sql = "DELETE FROM labels WHERE id = ?";
-//        String sql = "UPDATE labels SET status = 'DELETED' WHERE id = ?";
-//        try (PreparedStatement stmnt = DbConnectionUtils.getPreparedStatement(sql)) {
-//            stmnt.setLong(1, id);
-//            return stmnt.executeUpdate() != 0;
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            return false;
-//        }
     }
 }
